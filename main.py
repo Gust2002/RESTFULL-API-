1564e225446fb8abeb1fe39ce3ccc3d288c60b10
Gus Pellegrino <gustavopellegrinofialho25@gmail.com>
2025-10-28 16:50:08 +0000

    Adding structures into the code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -9,40 +11,42 @@ def Hello_world():
 
 my_library = {}
 
+class Book(BaseModel):
+    book_name = str 
+    author_name = str 
+    book_year = int
+
 @app.get("/request")
-def get_book(id_book: int, book_name: str, author_name: str, book_year: int):
+def get_book():
  if not my_library:
     return {"Message": "This book does not exist"}
  else:
      return {"get_book": my_library}
  
 @app.post("/create")
-def post_book(id_book: int, book_name: str, author_name: str, book_year):
+def post_book(id_book: int, book: Book):
     if id_book in my_library:
         raise HTTPException(status_code=400,detail="This book is created already")
     else:
-        my_library[id_book] = {"book_name": book_name, "author_name": author_name, "book_year": book_year}
-        
+        my_library[id_book] = book.dict()
         return {"Message": "Your book has been created with suceafully!!"}
     
+
 @app_put("/update/{id_book}")
-def put_book(id_book: int, book_name: str, author_name: str, book_year: int):
-    book = my_library.get[id_book]
-    if not book:
+def put_book(id_book: int, book: Book):
+    my_book = my_library.get[id_book]
+    if not my_book:
         raise HTTPException(status_code=404,detail="This book was not found on the system")
     else:
-        if book_name:
-         book_name["book_name"] = book_name
-        if author_name:
-         author_name["author_name"] = author_name
-        if book_year:
-         book_year["book_year"] = book_year
-    
-    return {"Message": "Your book has been update with sucesfully"} 
+        my_book[id_book] = book.dict()
+        return {"Message": "Your book has been update with sucesfully"} 
+
+
 
 @app.delete("/delete/{id_book}")
-def delete_book(id_book: int, book_name: str, author_book: str, year_book: int):
+def delete_book(id_book: int):
     if id_book not in my_library:
         raise HTTPException(status_code=500,detail="This book was not found on the system")
     else:
         del my_library[id_book]
+        return {"Message": "Your book has been deleted with sucesfully"}
